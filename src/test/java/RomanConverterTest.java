fd95a58283d6a1a73886e70a77382840d7e8e382
AdrianGoh <songleng93@hotmail.com>
2015-07-03 19:39:16 -0700

    updated

--- src/test/java/RomanConverterTest.java
+++ src/test/java/RomanConverterTest.java
@@ -4,7 +4,6 @@ import org.junit.Test;
 import static org.fest.assertions.Assertions.assertThat;
 
 public class RomanConverterTest {
-
     @Test public void itShouldConvertIto1(){
         assertThat(RomanConverter.Convert('I')).isEqualTo(1);
     }
